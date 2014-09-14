15bb9f2090f3c09462d1a53ee38cab1844fd3200
yuki3738 <y.minamiya@gmail.com>
2014-09-14 22:23:21 +0900

    i次は9-4-4から

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -7,4 +7,23 @@ class Admin::SessionsController < Admin::Base
       render action: 'new'
     end
   end
-end
+
+  def create
+    @form = Admin::LoginForm.new(params[:admin_login_form])
+    if @form.email.present?
+      administrator = Administrator.find_by(email_for_index: @form.email.downcase)
+    end
+    if Admin::Authenticator.new(administrator).authenticate(@form.password)
+      if administrator.suspended?
+        flash.now.alert = 'アカウントが停止されています。'
+        render action: 'new'
+      else
+        session[:administrator_id] = administrator.id
+        flash.notice = 'ログインしました。'
+        redirect_to :admin_root
+      end
+    else
+      flash.now.alert = 'メールアドレスまたはパスワードが正しくありません。'
+      render action: 'new'
+    end
+  end
