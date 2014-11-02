47e60c088ec8e293d4dbef1f1a07f10301a5582a
yuki3738 <y.minamiya@gmail.com>
2014-11-02 18:26:14 +0900

    次はP209 Chapter11 から

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
-config = Rails.application.config.baukis
+  config = Rails.application.config.baukis
 
-constraints host: config[:staff][:host] do
-  namespace :staff, path: config[:staff][:path] do
-    root 'top#index'
-    get 'login' => 'sessions#new', as: :login
-    resource :session, only: [ :create, :destroy ]
-    resource :account, except: [ :new, :create, :destroy ]
-  end
-end
-
-constraints host: config[:admin][:host] do
-  namespace :admin, path: config[:admin][:path]  do
-    root 'top#index'
-    get 'login' => 'sessions#new', as: :login
-    resource :session, only: [ :create, :destroy ]
-    resources :staff_members
-  end
-end
-
-  namespace :customer do
-    root 'top#index'
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :account, except: [ :new, :create, :destroy ]
+    end
   end
 
-  root 'errors#routing_error'
-  get '*anything' => 'errors#routing_error'
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path]  do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: [ :create, :destroy ]
+      resources :staff_members
+    end
+  end
+
+  constraints host: config[:customer][:host] do
+    namespace :customer, path: config[:customer][:path] do
+      root 'top#index'
+    end
+  end
+
+  root 'errors#not_found'
+  get '*anything' => 'errors#not_found'
 end
--- spec/routing/hostname_constraints_spec.rb
+++ spec/routing/hostname_constraints_spec.rb
@@ -17,17 +17,25 @@ describe 'ルーティング' do
     )
   end
 
+  example '顧客トップページ' do
+    expect(get: 'http://example.com/mypage').to route_to(
+      host: 'example.com',
+      controller: 'customer/top',
+      action: 'index'
+    )
+  end
+
   example 'ホスト名が対象外ならerrors/not_foundへ' do
     expect(get: 'http://foo.example.jp').to route_to(
       controller: 'errors',
-      action: 'routing_error'
+      action: 'not_found'
     )
   end
 
   example '存在しないパスならerrors/not_foundへ' do
     expect(get: 'http://baukis.example.com/xyz').to route_to(
       controller: 'errors',
-      action: 'routing_error',
+      action: 'not_found',
       anything: 'xyz'
     )
   end
